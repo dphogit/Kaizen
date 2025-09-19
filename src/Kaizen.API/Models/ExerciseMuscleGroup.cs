32abdbfe97cd56cac572c9ce3630cffae43f4037
Dean Phommahaxay <deanp21@gmail.com>
2025-09-19 21:51:36 +1200

    Add Exercise and MuscleGroup entities

---
+++ src/Kaizen.API/Models/ExerciseMuscleGroup.cs
@@ -0,0 +1,8 @@
+﻿namespace Kaizen.API.Models;
+
+/// <summary>The join entity for <see cref="Exercise"/> and <see cref="MuscleGroup"/>.</summary>
+public class ExerciseMuscleGroup
+{
+    public required int ExerciseId { get; set; }
+    public required string MuscleGroupCode { get; set; }
+}
---
+++ src/Kaizen.API/Models/MuscleGroup.cs
@@ -0,0 +1,7 @@
+﻿namespace Kaizen.API.Models;
+
+public class MuscleGroup
+{
+    public required string Code { get; set; }
+    public required string Name { get; set; }
+}
